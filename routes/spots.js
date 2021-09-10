c595b869f5db1233e213aa8b447f4f3830126414
Special K <kelian.somon@gmail.com>
2021-09-10 15:13:11 +0200

    last update

--- routes/spots.js
+++ routes/spots.js
@@ -1,7 +1,17 @@
 const router = require("express").Router();
-const Spots = require("../models/Spot.model");
+const Spot = require("../models/Spot.model");
 const bcrypt = require("bcryptjs");
 
+// routes/spots.routes.js
+// ... all imports stay unchanged
+
+// **** require Spot model in order to use it ****
+const Spot = require('../models/Spot.model');
+
+// ********* require fileUploader in order to use it *********
+const fileUploader = require('../config/cloudinary.config');
+
+//... all the routes stay unchanged
 
 
 module.exports = router;
