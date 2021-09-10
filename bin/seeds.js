5c213a5ba44f2bfa764ce9fdd4f193613129fdf0
Special K <kelian.somon@gmail.com>
2021-09-10 10:48:30 +0200

    bin created + login/signup pasted + login/signup route created + bcrypt installed

---
+++ bin/seeds.js
@@ -0,0 +1,18 @@
+// ℹ️ Connects to the database
+/* require("../db");
+const Book = require("../models/Book.model");
+
+const books = [
+  {
+    title: "The Hunger Games",
+    description:
+      "The Hunger Games is a 2008 dystopian novel by the American writer Suzanne Collins. It is written in the voice of 16-year-old Katniss Everdeen, who lives in the future, post-apocalyptic nation of Panem in North America. The Capitol, a highly advanced metropolis, exercises political control over the rest of the nation. The Hunger Games is an annual event in which one boy and one girl aged 12–18 from each of the twelve districts surrounding the Capitol are selected by lottery to compete in a televised battle royale to the death.",
+    author: "Suzanne Collins",
+    rating: 10,
+  },
+];
+
+Book.insertMany(books).then((booksFromDB) => {
+  console.log(`books created - ${booksFromDB.length}`);
+});
+ */
